07b184fa92afb976f70e86e1a2aa5efde5cebdcf
Davide Rizzo <sorcio@gmail.com>
2024-12-14 23:46:25 +0100

    day 14 faster

--- src/day14.rs
+++ src/day14.rs
@@ -1,103 +1,83 @@
-use std::str::FromStr;
-
 use aoc_runner_derive::{aoc, aoc_generator};
 use aoc_utils::{example_tests, known_input_tests};
 
-#[derive(Debug, Clone, Copy)]
-#[repr(align(2))]
-struct Vector {
-    x: i8,
-    y: i8,
+pub struct Robots {
+    position_x: Vec<u8>,
+    position_y: Vec<u8>,
+    velocity_x: Vec<u8>,
+    velocity_y: Vec<u8>,
 }
 
-impl Vector {
-    fn rem_euclid(self, modulo: Vector) -> Self {
-        Self {
-            x: self.x.rem_euclid(modulo.x),
-            y: self.y.rem_euclid(modulo.y),
-        }
+impl Robots {
+    fn iter(&self) -> impl Iterator<Item = (u8, u8, u8, u8)> + '_ {
+        self.position_x
+            .iter()
+            .zip(&self.position_y)
+            .zip(&self.velocity_x)
+            .zip(&self.velocity_y)
+            .map(|(((&px, &py), &vx), &vy)| (px, py, vx, vy))
     }
 }
 
-impl std::ops::Mul<i8> for Vector {
-    type Output = Self;
-
-    fn mul(self, rhs: i8) -> Self::Output {
-        Self {
-            x: self.x * rhs,
-            y: self.y * rhs,
-        }
-    }
+fn parse_vector<const WIDTH: u8, const HEIGHT: u8>(s: &str) -> (u8, u8) {
+    let mut parts = s.split(',');
+    let x: i8 = parts.next().unwrap().parse().unwrap();
+    let y: i8 = parts.next().unwrap().parse().unwrap();
+    (
+        (x as i16 + WIDTH as i16).rem_euclid(WIDTH as _) as u8,
+        (y as i16 + HEIGHT as i16).rem_euclid(HEIGHT as _) as u8,
+    )
 }
 
-impl std::ops::Add<Vector> for Vector {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
+pub fn parse_impl<const WIDTH: u8, const HEIGHT: u8>(input: &str) -> Robots {
+    let mut position_x = Vec::new();
+    let mut position_y = Vec::new();
+    let mut velocity_x = Vec::new();
+    let mut velocity_y = Vec::new();
+    for line in input.lines() {
+        let mut parts = line.split_whitespace();
+        let position_part = &parts.next().unwrap()[2..];
+        let velocity_part = &parts.next().unwrap()[2..];
+        let (px, py) = parse_vector::<WIDTH, HEIGHT>(position_part);
+        let (vx, vy) = parse_vector::<WIDTH, HEIGHT>(velocity_part);
+        position_x.push(px);
+        position_y.push(py);
+        velocity_x.push(vx);
+        velocity_y.push(vy);
     }
-}
 
-impl FromStr for Vector {
-    type Err = std::num::ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut parts = s.split(',');
-        Ok(Self {
-            x: parts.next().unwrap().parse()?,
-            y: parts.next().unwrap().parse()?,
-        })
+    Robots {
+        position_x,
+        position_y,
+        velocity_x,
+        velocity_y,
     }
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(align(4))]
-pub struct Robot {
-    position: Vector,
-    velocity: Vector,
 }
 
 #[aoc_generator(day14)]
-pub fn parse(input: &str) -> Vec<Robot> {
-    input
-        .lines()
-        .map(|line| {
-            let mut parts = line.split_whitespace();
-            let position_part = &parts.next().unwrap()[2..];
-            let velocity_part = &parts.next().unwrap()[2..];
-            let position = position_part.parse().unwrap();
-            let velocity = velocity_part.parse().unwrap();
-            Robot { position, velocity }
-        })
-        .collect()
+pub fn parse(input: &str) -> Robots {
+    parse_impl::<101, 103>(input)
 }
 
-fn part1_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usize {
-    let size = Vector {
-        x: WIDTH as i8,
-        y: HEIGHT as i8,
-    };
-    const ITERATIONS: i8 = 100;
+#[cfg(test)]
+fn parse_small_example(input: &str) -> Robots {
+    parse_impl::<11, 7>(input)
+}
 
-    let border_x: i8 = size.x / 2;
-    let border_y: i8 = size.y / 2;
+fn part1_impl<const WIDTH: u8, const HEIGHT: u8>(robots: &Robots) -> usize {
+    const ITERATIONS: u8 = 100;
+
+    let border_x: u8 = WIDTH / 2;
+    let border_y: u8 = HEIGHT / 2;
 
     let mut quadrant_nw = 0;
     let mut quadrant_ne = 0;
     let mut quadrant_sw = 0;
     let mut quadrant_se = 0;
 
-    for robot in robots {
-        let displacement = robot.velocity * ITERATIONS;
-        let new_position = (robot.position + displacement).rem_euclid(size);
-
-        let Vector {
-            x: position_x,
-            y: position_y,
-        } = new_position;
+    for (px, py, vx, vy) in robots.iter() {
+        let position_x = ((px as i32 + vx as i32 * ITERATIONS as i32) % WIDTH as i32) as u8;
+        let position_y = ((py as i32 + vy as i32 * ITERATIONS as i32) % HEIGHT as i32) as u8;
 
         // robots that are on the border are not counted
         if position_x < border_x && position_y < border_y {
@@ -119,16 +99,16 @@ fn part1_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usiz
 }
 
 #[cfg(test)]
-fn part1_small_example(input: &[Robot]) -> usize {
+fn part1_small_example(input: &Robots) -> usize {
     part1_impl::<11, 7>(input)
 }
 
 #[aoc(day14, part1)]
-pub fn part1(input: &[Robot]) -> usize {
+pub fn part1(input: &Robots) -> usize {
     part1_impl::<101, 103>(input)
 }
 
-fn part2_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usize {
+fn part2_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &Robots) -> usize {
     // check if robots draw a christmas tree figure
     // heuristic: is there a row/col that could look like the picture frame?
 
@@ -136,9 +116,8 @@ fn part2_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usiz
     let mut cols = [0u8; 104];
     debug_assert!(cols.len() >= WIDTH);
     'outer_x: loop {
-        for robot in robots {
-            let pos = (robot.position.x as i16 + robot.velocity.x as i16 * iterations_x as i16)
-                .rem_euclid(WIDTH as _) as usize;
+        for (px, _, vx, _) in robots.iter() {
+            let pos = (px as i16 + vx as i16 * iterations_x as i16).rem_euclid(WIDTH as _) as usize;
             cols[pos] += 1;
             if cols[pos] >= 32 {
                 break 'outer_x;
@@ -152,9 +131,9 @@ fn part2_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usiz
     let mut rows = [0u8; 104];
     debug_assert!(rows.len() >= HEIGHT);
     'outer_y: loop {
-        for robot in robots {
-            let pos = (robot.position.y as i16 + robot.velocity.y as i16 * iterations_y as i16)
-                .rem_euclid(HEIGHT as _) as usize;
+        for (_, py, _, vy) in robots.iter() {
+            let pos =
+                (py as i16 + vy as i16 * iterations_y as i16).rem_euclid(HEIGHT as _) as usize;
             rows[pos] += 1;
             if rows[pos] >= 32 {
                 break 'outer_y;
@@ -168,24 +147,16 @@ fn part2_impl<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usiz
 
     (1..WIDTH)
         .map(|i| (iterations_x as usize + i * WIDTH) - iterations_y as usize)
-        .filter(|n| *n % HEIGHT == 0)
-        .next()
+        .find(|n| *n % HEIGHT == 0)
         .unwrap()
         + iterations_y as usize
 }
 
-fn part2_impl_autovect<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]) -> usize {
-    let mut position_x = [0u8; 500];
-    let mut position_y = [0u8; 500];
-    let mut velocity_x = [0u8; 500];
-    let mut velocity_y = [0u8; 500];
-
-    for (i, robot) in robots.iter().enumerate() {
-        position_x[i] = (robot.position.x as i16 + WIDTH as i16).rem_euclid(WIDTH as _) as _;
-        position_y[i] = (robot.position.y as i16 + HEIGHT as i16).rem_euclid(HEIGHT as _) as _;
-        velocity_x[i] = (robot.velocity.x as i16 + WIDTH as i16).rem_euclid(WIDTH as _) as _;
-        velocity_y[i] = (robot.velocity.y as i16 + HEIGHT as i16).rem_euclid(HEIGHT as _) as _;
-    }
+fn part2_impl_autovect<const WIDTH: usize, const HEIGHT: usize>(robots: &Robots) -> usize {
+    let mut position_x = robots.position_x.clone();
+    let mut position_y = robots.position_y.clone();
+    let velocity_x = &robots.velocity_x;
+    let velocity_y = &robots.velocity_y;
 
     // check if robots draw a christmas tree figure
     // heuristic: is there a row/col that could look like the picture frame?
@@ -195,10 +166,10 @@ fn part2_impl_autovect<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]
     debug_assert!(cols.len() >= WIDTH);
     'outer_x: loop {
         for (pos, vel) in position_x.iter_mut().zip(velocity_x) {
-            *pos = (*pos + vel).rem_euclid(WIDTH as _).try_into().unwrap();
+            *pos = (*pos + vel).rem_euclid(WIDTH as _);
         }
         iterations_x += 1;
-        for pos in position_x {
+        for &pos in &position_x {
             cols[pos as usize] += 1;
             if cols[pos as usize] >= 32 {
                 break 'outer_x;
@@ -215,10 +186,10 @@ fn part2_impl_autovect<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]
             break;
         }
         for (pos, vel) in position_y.iter_mut().zip(velocity_y) {
-            *pos = (*pos + vel).rem_euclid(HEIGHT as _).try_into().unwrap();
+            *pos = (*pos + vel).rem_euclid(HEIGHT as _);
         }
         iterations_y += 1;
-        for pos in position_y {
+        for &pos in &position_y {
             rows[pos as usize] += 1;
             if rows[pos as usize] >= 32 {
                 break 'outer_y;
@@ -231,23 +202,23 @@ fn part2_impl_autovect<const WIDTH: usize, const HEIGHT: usize>(robots: &[Robot]
 
     (1..WIDTH)
         .map(|i| (iterations_x + i * WIDTH) - iterations_y)
-        .filter(|n| *n % HEIGHT == 0)
-        .next()
+        .find(|n| *n % HEIGHT == 0)
         .unwrap()
         + iterations_y
 }
 
 #[aoc(day14, part2, slow)]
-pub fn part2_slow(input: &[Robot]) -> usize {
+pub fn part2_slow(input: &Robots) -> usize {
     part2_impl::<101, 103>(input)
 }
 
 #[aoc(day14, part2)]
-pub fn part2(input: &[Robot]) -> usize {
+pub fn part2(input: &Robots) -> usize {
     part2_impl_autovect::<101, 103>(input)
 }
 
 example_tests! {
+    parser: crate::day14::parse_small_example,
     "
     p=0,4 v=3,-3
     p=6,3 v=-1,-3
@@ -272,4 +243,5 @@ known_input_tests! {
     input: include_str!("../input/2024/day14.txt"),
     part1 => 226236192,
     part2 => 8168,
+    part2_slow => 8168,
 }
